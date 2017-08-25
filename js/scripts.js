0de2b632b25720d2efc8dc1789344a279f5eee14
nv <nv781@yahoo.com>
2017-08-25 15:55:17 -0700

    add click functions to the html buttons

--- js/scripts.js
+++ js/scripts.js
@@ -101,8 +101,86 @@ var resetPizzaForm = function() {
   $('input[name="veggies"]:checked').attr("checked", false);
 }
 
+var populatePizzaList = function(pizza) {
+  $('.pizza-list').append('<div class="pizza">' +
+                            '<h4><span class="pizza-list-size">- One '+pizza.pizzaSize+' pizza</span></h4>' +
+                            '<div class="pizza-info-toggle">' +
+                              '<p>Cheese: <span class="pizza-list-cheese">'+pizza.cheese+'</span></p>' +
+                              '<p>Meat toppings: </p>' +
+                              '<ul class="pizza-list-meat-toppings"></ul>' +
+                              '<p>Veggie toppings: </p>' +
+                              '<ul class="pizza-list-veg-toppings"></ul>' +
+                              '<p>Cost of this pizza: $<span>'+pizza.cost.toFixed(2)+'</span></p>'+
+                            '</div>' +
+                          '</div>');
+  pizza.meatToppings.forEach(function(meatTopping) {
+    $('.pizza-list .pizza-list-meat-toppings').last().append('<li>'+meatTopping+'</li>');
+  });
+  pizza.vegToppings.forEach(function(vegTopping) {
+    $('.pizza-list .pizza-list-veg-toppings').last().append('<li>'+vegTopping+'</li>');
+  });
+
+  $('.pizza').last().click(function() {
+      $(this).find('.pizza-info-toggle').toggle();
+  });
+  $('.pizza-info-toggle').last().click(function() {
+      $(this).find('.pizza-info-toggle').toggle();
+  });
+
+}
+
+var populateTotalPrice = function(customerOrder) {
+  customerOrder.determineTotalCost();
+  return customerOrder.totalCost;
+}
 
 $(document).ready(function(){
+
+  var customerOrder = new Order();
+
+  // event handler for begin ordering button
+  $('.order-start button').click(function() {
+    nextDiv('.order-start', '.info-input');
+  });
+
+    // event handler for customer information submit
+  $('.infor-input form').submit(function(event) {
+    event.preventDefault();
+    customerOrder = createCustomerOrder();
+    nextDiv('.info-input', '.pizza-input');
+  });
+
+    // event handler for add pizza
+  $('.order-pizza-input form').submit(function(event) {
+    event.preventDefault();
+    var thisPizza = createPizza();
+    customerOrder.addPizza(thisPizza);
+    populatePizzaList(thisPizza);
+    $("#pizza-list-total-cost").text('$ '+ populateTotalPrice(customerOrder).toFixed(2));
+    nextDiv('.pizza-input', '.order-summary');
+  });
+
+    // event handler for add another pizza
+  $('#add-another-pizza').click(function() {
+    nextDiv('.order-summary', '.pizza-input');
+  });
+
+    // event handler for checkout order
+  $('#checkout-order').click(function() {
+    nextDiv('.order-summary', '.checked-out');
+  });
+
+    // event handler for new order/reset site
+  $('#new-order').click(function() {
+    customerOrder = new Order();
+    $('.pizza-list').empty();
+    nextDiv('.checked-out', '.order-start');
+  });
+
+    // event handler for log object to console
+  $('#console-log').click(function() {
+    console.log(customerOrder);
+  })
 
 
 });
